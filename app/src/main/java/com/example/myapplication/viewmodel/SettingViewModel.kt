c8187a3b971320c9f1c86a404e93b0068c9a842c
leokang123 <leokang123@github.com>
2025-05-22 16:52:59 +0900

    Reform ProtoType2-1-1 FCM 이랑 기능 멀지

--- app/src/main/java/com/example/myapplication/viewmodel/SettingViewModel.kt
+++ app/src/main/java/com/example/myapplication/viewmodel/SettingViewModel.kt
@@ -3,13 +3,9 @@ package com.example.myapplication.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import android.util.Log
-import com.example.myapplication.data.fcmtoken.FcmLogout
-import com.example.myapplication.network.ApiService
-import com.example.myapplication.repository.impl.MasterDataRepository
+import com.example.myapplication.repository.LoginRepository
 import com.example.myapplication.repository.impl.NotificationRepository
 import com.example.myapplication.utils.FirebaseTokenManager
-import com.example.myapplication.repository.LoginRepository
 import com.example.myapplication.utils.UserDataStore
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -18,7 +14,6 @@ import javax.inject.Inject
 @HiltViewModel
 class SettingsViewModel @Inject constructor(
     private val userDataStore: UserDataStore,
-    private val apiService: ApiService,
     private val notificationRepository: NotificationRepository,
     private val loginRepository: LoginRepository,
 ) : ViewModel() {
@@ -29,18 +24,15 @@ class SettingsViewModel @Inject constructor(
                 //FcmToken 삭제 기능 추가
                 val user = userDataStore.getUser()
                 val fcmToken = FirebaseTokenManager.getToken()
-                val token = userDataStore.getAccessToken()
-                
+
                 if (user != null && fcmToken != null) {
-                    notificationRepository.removeFcmToken(user.uuid ?:"", fcmToken)
-                    Log.d("SettingsViewModel", "FCM 토큰 삭제 요청 전송: uuid=${user.uuid}, token=$fcmToken")
+                    notificationRepository.removeFcmToken(user.uuid, fcmToken)
+                    Log.d(
+                        "SettingsViewModel",
+                        "FCM 토큰 삭제 요청 전송: uuid=${user.uuid}, token=$fcmToken"
+                    )
                 }
-                
-                
-                
-                if (!token.isNullOrEmpty()) {
-                    val res = apiService.signOut()
-                    if (res.isSuccessful) {
+
                 if (user != null) {
                     val res = loginRepository.logoutUser(user.uuid)
                     Log.d("LOGIN_BOOL", res.toString())
